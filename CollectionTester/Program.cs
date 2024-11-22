19ee71c7b3388a2d463490547422b68bd507104c
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2024-11-22 12:21:29 +0100

    array-indexer for MyList implemented, ready for IEnumerable

--- CollectionTester/Program.cs
+++ CollectionTester/Program.cs
@@ -7,7 +7,7 @@
   var listListString = new List<List<string>>();
 */
 
-var myList = new MyList();
+var myList = new MyList<int>();
 // Todo: Unsere Liste für alle Datentypen generalisieren!
 // eg. var myListListString = new MyList<MyList<string>>();
 
@@ -16,3 +16,50 @@ myList.Add(2); // index 1
 myList.Add(3); // index 2
 
 Console.WriteLine(myList.Get(1));
+
+var myStringList = new MyList<string>();
+myStringList.Add("Generics"); // index 0
+myStringList.Add("sind"); // index 1
+myStringList.Add("toll"); // index 2
+myStringList.Add(":)"); // index 3
+
+Console.WriteLine(myStringList.Get(3));
+
+var defaultList = new List<string>();
+defaultList.Add("1");
+defaultList.Add("2");
+defaultList.Add("3");
+defaultList.Add("3");
+/*
+  var x = defaultList.FindAll(
+    (item) => item == "3"); // result ["3", "3"]
+  defaultList.Distinct();
+*/
+
+// myStringList.Count = 42069; // private set restricts this...
+
+// Iteration durch myStringList, print all strings in list
+for (int i = 0; i < myStringList.Count; i++)
+{
+  Console.WriteLine(myStringList.Get(i));
+}
+
+Console.WriteLine("---Array-Indexer-implementiert---");
+
+for (int i = 0; i < myStringList.Count; i++)
+{
+  Console.WriteLine(myStringList[i]);
+}
+
+Console.WriteLine("---Array-Indexer-Setter-implementiert---");
+
+for (int i = 0; i < myStringList.Count; i++)
+{
+  myStringList[i] += ";";
+  Console.WriteLine(myStringList[i]);
+}
+
+// foreach (var x in myStringList)
+// {
+//
+// }
--- MyCollections/MyList.cs
+++ MyCollections/MyList.cs
@@ -1,48 +1,85 @@
 ﻿namespace MyCollections;
 
-internal class Node
+internal class Node<T>
 {
-  public int Value;
-  public Node? Next;
-  public Node(int value)
+  public T Value;
+  public Node<T>? Next;
+  public Node(T value)
   {
     Value = value;
     Next = null;
   }
 }
 
-public class MyList
+public class MyList<T>
 {
-  private Node? head;
-
-  public void Add(int value)
+  private Node<T>? head;
+  // private set restricts changes to Count to only this class
+  public int Count { get; private set; }
+  // array-indexer property => myList[1]
+  public T this[int index]
+  {
+    get
+    {
+      return this.Get(index);
+    }
+    set
+    {
+      this.Set(index, value);
+    }
+  }
+  public MyList()
+  {
+    Count = 0;
+  }
+  public void Add(T value)
   {
     if (head is null)
     {
-      head = new Node(value);
+      head = new Node<T>(value);
+      Count = 1;
     }
     else
     {
-      Node current = head;
+      Node<T> current = head;
       while (current.Next != null)
       {
         current = current.Next;
       }
 
-      current.Next = new Node(value);
+      current.Next = new Node<T>(value);
+      Count++;
     }
   }
-
-  public int Get(int index)
+  public T Get(int index)
   {
     int count = 0;
-    Node current = head;
+    Node<T>? current = head;
 
     while (current != null)
     {
       if (index == count)
       {
         return current.Value;
+      }
+      count++;
+      current = current.Next;
+    }
+
+    throw new IndexOutOfRangeException();
+  }
+
+  public void Set(int index, T value)
+  {
+    int count = 0;
+    Node<T>? current = head;
+
+    while (current != null)
+    {
+      if (index == count)
+      {
+        current.Value = value;
+        return;
       }
       count++;
       current = current.Next;
