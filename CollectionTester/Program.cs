201f73b24901fcb337cbe1d0cc88bee0c115cdcf
EvilWeasel <38261180+EvilWeasel@users.noreply.github.com>
2024-11-22 09:32:41 +0100

    Added todo for today

--- CollectionTester/Program.cs
+++ CollectionTester/Program.cs
@@ -1,6 +1,15 @@
 ﻿using MyCollections;
 
+/*
+  var intList = new List<int>();
+  var strList = new List<string>();
+  var decimalList = new List<decimal>();
+  var listListString = new List<List<string>>();
+*/
+
 var myList = new MyList();
+// Todo: Unsere Liste für alle Datentypen generalisieren!
+// eg. var myListListString = new MyList<MyList<string>>();
 
 myList.Add(1); // index 0
 myList.Add(2); // index 1
